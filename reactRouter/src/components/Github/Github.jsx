53531af5a2489e5b7275243b4adc01a613389404
Madhesh <madhesh.git@gmail.com>
2024-04-13 23:14:34 +0530

    modified components

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
+
 
 function Github() {
   
   const data = useLoaderData()
+ 
 
   return (
     <div className="text-center m-4 bg-black text-white p-4 text-3xl">
       Github Repos: {data.public_repos}
-    <img src={data.avatar_url} width={200} alt="" />
+      <img src={data.avatar_url} width={300} alt="" />
     </div>
   );
 }
@@ -17,6 +18,8 @@ export default Github
 
 export const githubInfoLoader = async () => {
   const response = await fetch("https://api.github.com/users/Madhesh21");
-
   return response.json()
 }
+
+
+
--- reactRouter/src/components/User/User.jsx
+++ reactRouter/src/components/User/User.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 
 function User() {
   const {userid} = useParams()
   return (
-    <div className='text-3xl text-black bg-orange-500 text-center py-5'>
+    <div className="text-3xl text-black bg-orange-500 text-center py-5">
       User: {userid}
     </div>
-  )
+  );
 }
 
 export default User
+
